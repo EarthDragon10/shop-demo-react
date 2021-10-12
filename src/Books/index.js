34530fbee0fb4b94c23c93db58660b67cc9dfee2
earthdragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-10-12 11:31:48 +0200

    preparazione per il refactoring

--- src/Books/index.js
+++ src/Books/index.js
@@ -5,21 +5,21 @@ import Book from "./Book";
 import Filters from "./Filters";
 
 export default function Books() {
-  const [books, setBooks] = useState(booksMock);
-  const [selectedFilter, setSelectedFilter] = useState("All");
+  // const [books, setBooks] = useState(booksMock);
+  // const [selectedFilter, setSelectedFilter] = useState("All");
 
-  const selectFilter = (filter) => {
-    setSelectedFilter(filter);
-    setBooks(
-      filter === "All"
-        ? booksMock
-        : booksMock.filter((book) => book.category === filter)
-    );
-  };
+  // const selectFilter = (filter) => {
+  //   setSelectedFilter(filter);
+  //   setBooks(
+  //     filter === "All"
+  //       ? booksMock
+  //       : booksMock.filter((book) => book.category === filter)
+  //   );
+  // };
 
   return (
     <Container fixed sx={{ mt: 4 }}>
-      <Filters selectedFilter={selectedFilter} selectFilter={selectFilter} />
+      <Filters />
       <Grid container spacing={2}>
         {books?.map((book) => (
           <Book key={book.id} {...book} />
