2f6f8e97037e29a6625c93fcb0eddbc0e8dc7193
earthdragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-10-12 12:58:47 +0200

    adds logic of filter in the reducer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,47 @@ import { useReducer, createContext } from "react";
 import Basket from "./Basket";
 import Books from "./Books";
 import Navigation from "./Navigation";
+import books from "./mocks/books"
 
-const INITIAL_STATE = { opened: false };
+const INITIAL_STATE = {
+  basket: {
+    items: [],
+    totalPrice: 0,
+    quantity: 0,
+    opened: false,
+  },
+  filters: {
+    filteredBooks: [],
+    word: "",
+    category: "All",
+  },
+  books: {
+    books,
+    categories: ["All", "Design", "Mobile", "Ux", "DevOps", "Essentials"],
+  },
+};
 
 function reducer(state, action) {
 
   switch(action.type) {
 
     case "TOGGLE":
-      return { ...state, opened: !state.opened};
+      return { ...state, basket: {
+        ...state.basket,
+        opened: !state.basket.opened
+      }};
+    
+    case "FILTER":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          filteredBooks: state.filters.categories === "All"
+          ? state.books.books
+          : state.books.books.filter((book) => book.category === state.filters.categories),
+          
+        }
+      }
     
     default:
       return state;
